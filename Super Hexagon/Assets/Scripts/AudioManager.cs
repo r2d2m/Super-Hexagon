ec7a645894cd5ccc9addbf83700f058c6298a186
shubham-saudolla <shubhamsaudolla@gmail.com>
2018-11-10 22:55:59 +0530

    Added an audio manager

---
+++ Super Hexagon/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,56 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    // the array that holds all the sounds
+    public Sound[] sounds;
+
+    // for a singleton pattern
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Start()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sounds => sounds.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("Sound " + name + " not found.");
+        }
+
+        s.source.Play();
+    }
+}
---
+++ Super Hexagon/Assets/Scripts/Sound.cs
@@ -0,0 +1,23 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+    public bool loop;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    [HideInInspector]
+    public AudioSource source;
+}
