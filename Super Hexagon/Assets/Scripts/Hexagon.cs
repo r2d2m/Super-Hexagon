79ad00bc64d7d48ddb5cb6e7fa5d39d8b84f4325
shubham-saudolla <shubhamsaudolla@gmail.com>
2018-11-10 11:09:02 +0530

    Set up hexagon spawning

---
+++ Super Hexagon/Assets/Scripts/Hexagon.cs
@@ -0,0 +1,32 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hexagon : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float shrinkSpeed = 3f;
+
+
+    void Start()
+    {
+        rb.rotation = Random.Range(0, 360);
+        transform.localScale = Vector3.one * 17f;
+    }
+
+    void Update()
+    {
+        transform.localScale -= Vector3.one * shrinkSpeed * Time.deltaTime;
+
+        if (transform.localScale.x <= 0.05f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Super Hexagon/Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float spawnRate = 1f;
+
+    public GameObject hexagonPrefab;
+
+    public float nextTimeToSpawn = 0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Time.time >= nextTimeToSpawn)
+        {
+            Instantiate(hexagonPrefab, Vector3.zero, Quaternion.identity);
+            nextTimeToSpawn = Time.time + 1 / spawnRate;
+        }
+    }
+}
