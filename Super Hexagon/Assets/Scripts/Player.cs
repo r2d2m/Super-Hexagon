d38cd251a02285df745ff0a5596880ca234c7857
shubham-saudolla <shubhamsaudolla@gmail.com>
2018-11-10 10:35:48 +0530

    Set up player movement

---
+++ Super Hexagon/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movementSpeed = 600f;
+
+    float movement = 0f;
+
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.deltaTime * -movementSpeed);
+    }
+}
